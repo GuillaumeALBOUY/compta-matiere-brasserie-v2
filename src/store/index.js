fa9f110be06a09a0b0be8e616ac6eaecd86a68b7
GuillaumeAlbouy <guillaume.albouy82@gmail.com>
2021-08-22 19:22:03 +0200

    Correction du bug de raffraichissement de la liste mp ajout du détail des mouvements de mp

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Vue from 'vue'
 import api from '../config/url.js'
 
 
@@ -6,37 +7,52 @@ export default {
   state: {
     nomBrasserie: "L'embrasserie",
     matieresPremieres: [],
-    
+    detailMatierePremiere: null
+
   },
   getters: {
     allMatieresPremieres: (state) => state.matieresPremieres
   },
   mutations: {
     SET_MATIERES_PREMIERES(state, matieresPremieres) {
-      state.matieresPremieres = matieresPremieres
+      //state.matieresPremieres = matieresPremieres;
+      Vue.set(state, 'matieresPremieres', matieresPremieres);
+    },
+    ADD_MATIERES_PREMIERES(state, matierePremiere) {
+      state.matieresPremieres.push(matierePremiere);
+    },
+    SET_DETAIL_MATIERE_PREMIERE (state, matierePremiere) {
+      state.detailMatierePremiere=matierePremiere;
     }
   },
   actions: {
     getMatieresPremieres({ commit }) {
-      console.log('Récupération des données de connexion : ' + api + ' - ' + api.urlBackEnd);
       axios.get(api.urlBackEnd + ':' + api.portBackEnd + '/api/stockmp/')
         .then(response => {
           commit('SET_MATIERES_PREMIERES', response.data)
         })
     },
 
+    getMatierePremiere({ commit }, payload ) {
+      axios.get(api.urlBackEnd + ':' + api.portBackEnd + '/api/stockmp/'+ payload.matierePremiere.nom)
+            .then(response => { console.log("matiere premiere obtenue : "+response);
+            commit('SET_DETAIL_MATIERE_PREMIERE',response.data)})
+
+    },
+
     setMatierePremiere({ commit }, payload) {
-      const body= { matiere : payload.matiere};
-      console.log("Demande d'ajout de : " +   body.matiere.nom);
-      axios.post( api.urlBackEnd + ':' + api.portBackEnd + '/api/stockmp/', body )
-        .then(axios.get(api.urlBackEnd + ':' + api.portBackEnd + '/api/stockmp/') // TODO : partie de code à factoriser avec plus haut
+      const body = { matiere: payload.matiere };
+      axios.post(api.urlBackEnd + ':' + api.portBackEnd + '/api/stockmp/', body)
+        .then(
+          /*axios.get(api.urlBackEnd + ':' + api.portBackEnd + '/api/stockmp/') // TODO : partie de code à factoriser avec plus haut
           .then(response => {
-          commit('SET_MATIERES_PREMIERES', response.data)
-        }));
+          commit('SET_MATIERES_PREMIERES', response.data)})*/
+          commit('ADD_MATIERES_PREMIERES', body.matiere)
+        );
 
     }
 
   },
-modules: {
-}
+  modules: {
+  }
 }
